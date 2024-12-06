3a64fb64237c3459b2da8bda2f79a6e56c4fdfad
sunitha <gsita3251@gmail.com>
2024-12-06 19:24:53 +0530

    two files created

---
+++ js.js
@@ -0,0 +1,73 @@
+// var a=["Alice", "Bob", "Charlie", "David"]
+// console.log(a)
+// a.unshift("abc");
+// console.log(a);
+// a.pop("");
+// console.log(a)
+// a.includes("Bob");
+// console.log(a);
+
+
+// var arr="Learn,Practice,Improve"
+// console.log(arr);
+// arr.split("");
+// console.log(arr);
+// console.log(arr.split("").reverse().join(" "));
+// console.log(arr);
+
+// var d=["Song1", "Song2", "Song3"];
+// var c=["Song4","Song5"];
+// console.log(d);
+// console.log(d.concat(c));
+// d.shift();
+// console.log(d);
+// var e=(d.concat(c))
+// console.log(e.shift());
+// console.log(e)
+// e.push("song6")
+// console.log(e)
+
+
+// var d=["Inception", "Titanic", "Avatar", "Interstellar", "Gladiator"]
+// console.log(d.slice(1,4))
+// var f=d.slice(1,4)
+// var s=["The Matrix", "The Godfather"]
+// console.log(f.concat(s).reverse())
+
+// var g="John,Doe,Jane,Smith"
+// console.log(g.split())
+// console.log(g.includes("Jane"))
+// var h=g.split()
+// h.reverse()
+// console.log(h)
+
+// var n=[1, 2, 3, 4, 5];
+// console.log(n)
+// n.shift()
+// console.log(n)
+// n.push(6,7)
+// console.log(n.length)
+
+// var y=["Milk", "Bread", "Eggs", "Butter"]
+// console.log(y.includes("Eggs"))
+// y.pop()
+// console.log(y)
+// var k=["juice","cheese"]
+// console.log(y.concat(k))
+
+
+// var r=["Alice", "Bob", "Charlie"] 
+// var o= ["David", "Eve"]
+// console.log(r.reverse().concat(o))
+// var t=r.reverse().concat(o)
+// t.unshift("Frank")
+// console.log(t)
+
+// var w=["Milk", "Bread", "Eggs", "Butter"]
+// var z=w.indexOf("Eggs")
+// console.log(z)
+// w[z]="cheese";
+// console.log(w)
+// w.unshift("juice")
+// console.log(w)
+
